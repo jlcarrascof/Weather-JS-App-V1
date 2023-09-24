18715e4a9423110fa7acfb2ae43f21363032327a
jlcarrascof <entrenamientospuntonet@gmail.com>
2023-09-24 11:13:58 -0400

    Add Javascript to get the data from API

--- app.js
+++ app.js
@@ -4,3 +4,18 @@ var name = document.querySelector('.name');
 var desc = document.querySelector('.desc');
 var temp = document.querySelector('.temp');
 
+button.addEventListener('click', function(){
+    fetch('https://api.openweathermap.org/data/2.5/weather?q='+inputValue.value+'&appid=2111305ac2f01df876531a95935704f7')
+    .then(response => response.json())
+    .then(data => {
+        var nameValue = data['name'];
+        var tempValue = data['main']['temp'];
+        var descValue = data['weather'][0]['description'];
+        
+        name.innerHTML = nameValue;
+        temp.innerHTML = tempValue;
+        desc.innerHTML = descValue;
+    })
+    .catch(err => alert("Wrong city name!"));
+});
+
